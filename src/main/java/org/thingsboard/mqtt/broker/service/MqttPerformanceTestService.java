fc1db88aba5399a9ed35ef26326228ad7535e577
vzikratyi <vzikratyi@thingsboard.io>
2021-01-14 13:25:55 +0200

    Fixed test and added stats

--- src/main/java/org/thingsboard/mqtt/broker/service/MqttPerformanceTestService.java
+++ src/main/java/org/thingsboard/mqtt/broker/service/MqttPerformanceTestService.java
@@ -1,20 +1,24 @@
 package org.thingsboard.mqtt.broker.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 import org.eclipse.paho.client.mqttv3.MqttAsyncClient;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -23,10 +27,12 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Service
 @Slf4j
 public class MqttPerformanceTestService {
-    private static final int SUBSCRIBERS = 20;
-    private static final int PUBLISHERS = 300;
-    private static final int MSGS_TO_PUBLISH = 200;
-    private static final int MSGS_PER_SECOND = 20;
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    private static final int SUBSCRIBERS = 5;
+    private static final int PUBLISHERS = 20;
+    private static final int MSGS_TO_PUBLISH = 1000;
+    private static final int MAX_MSGS_PER_SECOND = 100;
 
     private static final String TOPIC_PREFIX = "europe/ua/kyiv/tb/";
 
@@ -41,45 +47,87 @@ public class MqttPerformanceTestService {
     @PostConstruct
     public void init() throws Exception {
         CountDownLatch subscribersCDL = new CountDownLatch(SUBSCRIBERS);
+        List<SubscriberInfo> subscribers = new ArrayList<>(SUBSCRIBERS);
+
+        DescriptiveStatistics generalLatencyStats = new DescriptiveStatistics();
+
+        log.info("Start connecting subscribers.");
         for (int i = 0; i < SUBSCRIBERS; i++) {
-            MqttClient subClient = new MqttClient("tcp://" + mqttHost + ":" + mqttPort, "test_sub_client_" + i);
+            int subscriberId = i;
+            MqttClient subClient = new MqttClient("tcp://" + mqttHost + ":" + mqttPort, "test_sub_client_" + i, new MemoryPersistence());
             subClient.connect();
-            String topicFilter = i % 2 == 0 ? TOPIC_PREFIX + "+" : TOPIC_PREFIX + "#";
+            subscribers.add(new SubscriberInfo(subClient, subscriberId));
+            String topicFilter = subscriberId % 2 == 0 ? TOPIC_PREFIX + "+" : TOPIC_PREFIX + "#";
+
             AtomicInteger receivedMsgs = new AtomicInteger(0);
-            subClient.subscribe(topicFilter, (topic, message) -> {
-                if (receivedMsgs.incrementAndGet() >= PUBLISHERS * MSGS_TO_PUBLISH) {
-                    subClient.disconnect();
-                    subClient.close();
-                    subscribersCDL.countDown();
+            subClient.subscribe(topicFilter, (topic, mqttMessage) -> {
+                try {
+                    Message message = mapper.readValue(mqttMessage.getPayload(), Message.class);
+                    long now = System.currentTimeMillis();
+                    generalLatencyStats.addValue(now - message.createTime);
+                    if (receivedMsgs.incrementAndGet() >= PUBLISHERS * MSGS_TO_PUBLISH) {
+                        subscribersCDL.countDown();
+                    }
+                } catch (Exception e) {
+                    log.error("[{}] Failed to process msg {}", subscriberId, mqttMessage);
                 }
             });
         }
+        log.info("Finished connecting subscribers.");
 
+        log.info("Start connecting publishers.");
         List<PublisherInfo> publishers = new ArrayList<>(PUBLISHERS);
         for (int i = 0; i < PUBLISHERS; i++) {
-            MqttAsyncClient pubAsyncClient = new MqttAsyncClient("tcp://" + mqttHost + ":" + mqttPort, "test_pub_client_" + i);
-            pubAsyncClient.connect();
+            MqttAsyncClient pubAsyncClient = new MqttAsyncClient("tcp://" + mqttHost + ":" + mqttPort, "test_pub_client_" + i,
+                    new MemoryPersistence());
+            pubAsyncClient.connect().waitForCompletion();
             String topic = TOPIC_PREFIX + i;
             publishers.add(new PublisherInfo(pubAsyncClient, i, topic));
         }
+        log.info("Finished connecting publishers.");
+
+        AtomicInteger publishedMsgsPerPublisher = new AtomicInteger();
         Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(() -> {
+            if (publishedMsgsPerPublisher.getAndIncrement() >= MSGS_TO_PUBLISH) {
+                return;
+            }
             for (PublisherInfo publisherInfo : publishers) {
                 try {
-                    publisherInfo.publisher.publish(publisherInfo.topic, new MqttMessage(TEST_MESSAGE.getBytes()));
-                } catch (MqttException e) {
-                    log.error("[{}] Failed to publish", publisherInfo.id);
+                    Message message = new Message(System.currentTimeMillis());
+                    MqttMessage mqttMessage = new MqttMessage(mapper.writeValueAsBytes(message));
+                    publisherInfo.publisher.publish(publisherInfo.topic, mqttMessage);
+                } catch (MqttException | JsonProcessingException e) {
+                    log.error("[{}] Failed to publish", publisherInfo.id, e);
                 }
             }
-        }, 0, 1000 / MSGS_PER_SECOND, TimeUnit.MILLISECONDS);
+        }, 0, 1000 / MAX_MSGS_PER_SECOND, TimeUnit.MILLISECONDS);
 
-        subscribersCDL.await(MSGS_TO_PUBLISH / MSGS_PER_SECOND + 5, TimeUnit.SECONDS);
+        boolean successfullyProcessed = subscribersCDL.await(MSGS_TO_PUBLISH / MAX_MSGS_PER_SECOND + 5, TimeUnit.SECONDS);
+        if (!successfullyProcessed) {
+            log.error("Timeout waiting for subscribers to process messages.");
+        }
+        log.info("Disconnecting subscribers.");
+        for (SubscriberInfo subscriberInfo : subscribers) {
+            try {
+                subscriberInfo.subscriber.disconnect();
+            } catch (MqttException e) {
+                log.error("[{}] Failed to disconnect subscriber", subscriberInfo.id);
+            }
+        }
+
+        log.info("Disconnecting publishers.");
         for (PublisherInfo publisherInfo : publishers) {
             try {
                 publisherInfo.publisher.disconnect();
             } catch (MqttException e) {
-                log.error("[{}] Failed to disconnect", publisherInfo.id);
+                log.error("[{}] Failed to disconnect publisher", publisherInfo.id);
             }
         }
+
+        log.info("Latency stats: avg - {}, median - {}, max - {}, min - {}, 95th - {}.",
+                generalLatencyStats.getSum() / generalLatencyStats.getN(),
+                generalLatencyStats.getMean(), generalLatencyStats.getMax(),
+                generalLatencyStats.getMin(), generalLatencyStats.getPercentile(95));
     }
 
     @AllArgsConstructor
@@ -88,5 +136,19 @@ public class MqttPerformanceTestService {
         private final MqttAsyncClient publisher;
         private final int id;
         private final String topic;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    private static class SubscriberInfo {
+        private final MqttClient subscriber;
+        private final int id;
+    }
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Data
+    private static class Message {
+        private long createTime;
     }
 }
